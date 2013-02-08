03b30124d5b3385a50005db5f5dc7bca3f5a4119
Patrick Heneise <patrick.heneise@gmail.com>
2013-02-08 18:44:18 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+function getTweets() {
+  $(".tweets").miniTwitter({
+    query: "#sportshackday OR @sportshackday OR sportshackday.com",
+    limit: 8
+  });
+}
+
+// populate tweets imperatively
+getTweets();
+
+// timer to update tweets every 10 seconds
+setInterval(function(){ getTweets(); }, 10000);
+});
